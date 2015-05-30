2b120adcc963c2f07665cf9e11377bf9042d2b6e
Jacqueline M Ali <dragonperson@gmail.com>
2015-05-30 00:45:19 -0400

    Connect Firebase
    Figured out how to store some basic currencies. Next steps: design flow
    of program, design data format, and figure out where to eliminate hard
    coding/repitition (e.g. where will you iterate? Where should you be
    mapping things configurably?)

--- js/resultpublisher.js
+++ js/resultpublisher.js
@@ -1,11 +1,79 @@
 
 
+// JSONRates Exchange rate API
 JR.apikey('jr-c945036558279aed9ad4046e90a75457');
+// Firebase for storage
+var rootRef = new Firebase("blinding-heat-4617.firebaseIO.com");
+var galaMoneyWallRef = rootRef.child('bill_list');
+
+// Current GalaWall:
+/* 
+Honduras: 2 and 10
+Fiji: 5
+GBP: 5 and 10
+China: 50 and 10
+- Euro 5 and 10
+hong kong 20
+turkey 20 and 10
+GBP: 20
+Israel: 20
+India: 500 100 50
+Singapore: 2 and 5
+- Canada 20
+- USD: 2 and 2
+- Canada: 5 and 5
+*/
+
+// Map countries/names to their symbols here!
+/* var currencyNames = {
+
+} */
+
+galaMoneyWallRef.push({
+	currency : "EUR",
+	value: 5
+});
+
+galaMoneyWallRef.push({
+	currency : "EUR",
+	value: 10
+});
+
+galaMoneyWallRef.push({
+	currency : "USD",
+	value: 2
+});
+
+galaMoneyWallRef.push({
+	currency : "USD",
+	value: 2
+});
+
+galaMoneyWallRef.push({
+	currency : "CAD",
+	value: 5
+});
+
+galaMoneyWallRef.push({
+	currency : "CAD",
+	value: 5
+});
+
+galaMoneyWallRef.push({
+	currency : "CAD",
+	value: 20
+});
 
 var main = function () {
-	JR.from('CAD').to('USD').get(function(result){
+	/*bill_collection.set({
+		five_euro: {
+			currency: "EUR",
+			value: 5;		
+		}
+	});*/
+	/*JR.from('CAD').to('USD').get(function(result){
 		$('.moneywall-value').text(result.rate);
-	});
+	});*/
 	
 }
 
